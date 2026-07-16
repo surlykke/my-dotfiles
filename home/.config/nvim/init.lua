d22a93cde323ab8e7c9daf59d67c99c6a9140d01
Christian Surlykke <christian@surlykke.dk>
2026-07-16 08:59:29 +0200

    nvim, initlua: Add venn plugin for ascii graphics

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -120,6 +120,31 @@ require('oil').setup({
     },
 })
 
+-- Graphics
+vim.pack.add({'https://github.com/jbyuki/venn.nvim'})
+local function toggle_graphics_mode()
+    local venn_enabled = vim.inspect(vim.b.venn_enabled)
+    if venn_enabled == "nil" then
+        vim.b.venn_enabled = true
+        vim.cmd[[setlocal ve=all]]
+        -- draw a line on HJKL keystokes
+        vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", {noremap = true})
+        -- draw a box by pressing "f" with visual selection
+        vim.api.nvim_buf_set_keymap(0, "v", "b", ":VBox<CR>", {noremap = true})
+    else
+        vim.cmd[[setlocal ve=]]
+        vim.api.nvim_buf_del_keymap(0, "n", "J")
+        vim.api.nvim_buf_del_keymap(0, "n", "K")
+        vim.api.nvim_buf_del_keymap(0, "n", "L")
+        vim.api.nvim_buf_del_keymap(0, "n", "H")
+        vim.api.nvim_buf_del_keymap(0, "v", "b")
+        vim.b.venn_enabled = nil
+    end
+end
+vim.keymap.set('n', '<leader>g', toggle_graphics_mode, { desc = 'Toggle graphics mode' })
 
 -- which-key
 vim.pack.add({ 'https://github.com/folke/which-key.nvim' })
